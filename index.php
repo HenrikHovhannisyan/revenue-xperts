e717c28687585e3d71de4946ff0364a7a9943a81
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-12-13 17:03:19 +0400

    Added Projects Spotlight

--- index.php
+++ index.php
@@ -5,7 +5,56 @@
 </div>
 
 <main>
-
+    <section id="projects-spotlight">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-6 projects-spotlight-container">
+                    <h2 class="projects-spotlight-title">Projects Spotlight</h2>
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Situation
+                            </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            In the fiercely competitive UK SaaS ConTech sector, a sustainability-focused startup urgently needed to secure Series A funding to extend their cash runway and drive growth. With over half of their MRR at risk of churn within six months, the stakes were high.
+                            </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Action
+                            </button>
+                            </h2>
+                            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            In the fiercely competitive UK SaaS ConTech sector, a sustainability-focused startup urgently needed to secure Series A funding to extend their cash runway and drive growth. With over half of their MRR at risk of churn within six months, the stakes were high.
+                            </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Result
+                            </button>
+                            </h2>
+                            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            In the fiercely competitive UK SaaS ConTech sector, a sustainability-focused startup urgently needed to secure Series A funding to extend their cash runway and drive growth. With over half of their MRR at risk of churn within six months, the stakes were high.
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 text-center">
+                    <img src="./assets/images/projects-spotlight.png" class="img-fluid" alt="Projects Spotlight">
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php include 'includes/footer.php'; ?>
