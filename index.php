640a8113f3c80d391b7e1bcf2ac5860fb51d0f61
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-12-13 17:28:15 +0400

    Added Price

--- index.php
+++ index.php
@@ -55,6 +55,51 @@
             </div>
         </div>
     </section>
+
+    <section id="price">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-lg-4">
+                    <h3>YOY</h3>
+                    <h2>65%+</h2>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Bridge MRR Gaps</li>
+                        <li class="list-group-item">Predicable ARR</li>
+                        <li class="list-group-item">World-Class Rev Ops Systems & Processes</li>
+                        <li class="list-group-item">KPIs & Reporting</li>
+                        <li class="list-group-item">Data-Driven Decisions</li>
+                        <li class="list-group-item">Board & Investor Confidence</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-lg-4 arr">
+                    <div class="arr-container">
+                        <h3>ARR</h3>
+                        <h2>$30 M</h2>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Get to $1M ARR in 12 months</li>
+                            <li class="list-group-item">Scale to $30M</li>
+                            <li class="list-group-item">Innovative Multi-Channel GTM Strategy</li>
+                            <li class="list-group-item">Science-based Sales Process</li>
+                            <li class="list-group-item">Coaching & Development</li>
+                            <li class="list-group-item">Middle East Market Expansion</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <h3>OVERALL</h3>
+                    <h2>$135 M+</h2>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">UK, US & EMEA</li>
+                        <li class="list-group-item">ConTech</li>
+                        <li class="list-group-item">PropTech</li>
+                        <li class="list-group-item">GreenTech</li>
+                        <li class="list-group-item">ERP, EAM</li>
+                        <li class="list-group-item">Project Collaboration</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php include 'includes/footer.php'; ?>
