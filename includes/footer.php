abacea875d916dc9594224717908627a5c07d2fc
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-12-17 17:40:30 +0400

    Added footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,93 @@
 <footer>
-<!--    <p>&copy; --><?//= date("Y"); ?><!-- Revenue-Xperts. All rights reserved.</p>-->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="top-box">
+                <img src="../assets/images/footer-logo.png" class="img-fluid" alt="footer-logo">
+                <a href="">
+                    <i class="fa-solid fa-arrow-right"></i>
+                </a>
+            </div>
+
+            <div class="col-md-5 mb-3">
+                <img src="../assets/images/small-logo.png" class="img-fluid" alt="footer-logo">
+                <p class="text-white mt-3">
+                    We manage lead logistices <br>
+                    for worlds multinational <br>
+                    companies. <br>
+                </p>
+            </div>
+
+            <div class="col-6 col-md-2 mb-3 text-white">
+                <h5>Company</h5>
+                <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Founder & CEO</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">SaaS Sales Leaders</a></li>
+                </ul>
+            </div>
+
+            <div class="col-6 col-md-2 mb-3 text-white">
+                <h5>Contact</h5>
+                <ul class="nav flex-column">
+                <li class="nav-item mb-2">
+                1498w Fluton ste, STE <br>
+                2D Chicgo, IL 63867.
+                </li>
+                <li class="nav-item mb-2"><a href="tel:(123)456789000" class="nav-link p-0 text-white">(123) 456789000</a></li>
+                <li class="nav-item mb-2"><a href="mailto:info@bellrings.com" class="nav-link p-0 text-white">info@bellrings.com</a></li>
+                </ul>
+            </div>
+
+            <div class="col-6 col-md-2 mb-3 offset-md-1 text-white">
+                <h5>Follow us on Social</h5>
+                <div class="social">
+                    <a href="">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="">
+                        <i class="fa-brands fa-x-twitter"></i>
+                    </a>
+                    <a href="">
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top text-white">
+            <p>©<?= date("Y"); ?> Bell Rings – ALL RIGHTS RESERVED.</p>
+            <ul class="list-unstyled d-flex">
+                <li class="ms-3 me-3">
+                    <a class="text-white" href="#">
+                    Privacy Policy
+                    </a>
+                </li>
+                |
+                <li class="ms-3 me-3">
+                    <a class="text-white" href="#">
+                    Terms & Conditions
+                    </a>
+                </li>
+                |
+                <li class="ms-3 me-3">
+                    <a class="text-white" href="#">
+                    Cookies Policy
+                    </a>
+                </li>
+                |
+                <li class="ms-3">
+                    <a class="text-white" href="#">
+                    Map
+                    </a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
